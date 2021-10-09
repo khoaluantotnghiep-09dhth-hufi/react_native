ac36fd0a4ffb8051646f2de3654cf3bb8e5e281c
lqt2k <wintrung01@gmail.com>
2021-10-10 01:43:54 +0700

    update nhẹ :V

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -2,4 +2,12 @@
 export const API_URL = 'http://10.0.3.2:8000';
 
 //ADV
-//export const API_URL = 'http://192.168.1.13:8000';
+//export const API_URL = 'http://10.0.2.2:8000';
+//export const API_URL = 'http://localhost:8000';
+//export const API_URL = 'http://10.0.2.2:3000';
+//export const API_URL = 'http://localhost:3000';
+//export const API_URL = 'http://127.0.0.1:3000';	
+//export const API_URL = 'http://127.0.0.1:8000';
+
+//Ngrok
+//export const API_URL = 'http://f913-1-53-112-176.ngrok.io/';
