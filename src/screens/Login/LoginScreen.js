676abfc753e73ddf20c56d145ae5479c20f03b2f
lqt2k <wintrung01@gmail.com>
2021-10-13 17:09:00 +0700

    Cập nhập giao diện đăng nhập

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,51 +1,64 @@
 import React, { Component } from "react";
-
-import styles from "./style";
 import { Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Image, TouchableOpacity, Platform, StatusBar, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
-
-const appId = "1047121222092614"
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Entypo } from '@expo/vector-icons';
 
 export default class LoginScreen extends Component {
 
   render() {
     return (
-      <>
-        <KeyboardAvoidingView style={styles.containerView} behavior="padding">
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.loginScreenContainer}>
-              <View style={styles.loginFormView}>
-                <Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ877ZrGb6pm9tr8T_9SmYyDZeQbg7biO--eA&usqp=CAU' }} />
-                <Text style={styles.logoText}>Đăng Nhập Uniqlo</Text>
+      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.loginScreenContainer}>
+            <View style={styles.loginFormView}>
+              <View>
+                <Image  
+                  style={styles.tinyLogo}
+                  source={{uri: 'https://brandslogo.net/wp-content/uploads/2014/10/Uniqlo-logo.png'}} />
+                <Text style={styles.logoText}>UNIQLO</Text>
+                <Text style={styles.logoText2}>This is <Text style={{color:"red"}}>LifeWear</Text></Text>
+              </View>
+              <View>
                 <TextInput
                   placeholder="Số điện thoại"
                   placeholderColor="#c4c3cb"
+                  keyboardType="numeric"
+                  maxLength={11}
+                  minLength={10}
+                  textAlign={'center'}
                   style={styles.loginFormTextInput} />
+                <Entypo name="phone" size={25} color="red" style={styles.iconStyle} />
+              </View>
+              <View>
                 <TextInput
                   placeholder="Mật khẩu"
                   placeholderColor="#c4c3cb"
-                  maxlength={11}
-                  minlength={10}
-                  size={10}
                   pattern="^[0-9]*$"
+                  password={true} 
+                  textAlign={'center'}
                   style={styles.loginFormTextInput}
                   secureTextEntry={true} />
+                <Entypo name="key" size={25} color="red" style={styles.iconStyle} />
+              </View>
+              <View>
                 <Button
                   buttonStyle={styles.loginButton}
                   onPress={() => this.onLoginPress()}
                   title="Đăng Nhập"
                 />
-                {/* <Button
-                buttonStyle={styles.fbLoginButton}
-                onPress={() => this.onFbLoginPress()}
-                title="Login with Facebook"
-                color="#3897f1"
-              /> */}
+              </View>
+              <View>
+              <Button
+                  buttonStyle={styles.loginButton}
+                  onPress={() => this.onLoginPress()}
+                  title="Đăng Ký"
+                />
               </View>
             </View>
-          </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>
-      </>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
 
@@ -59,17 +72,70 @@ export default class LoginScreen extends Component {
 
   }
 
-  async onFbLoginPress() {
-    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(appId, {
-      permissions: ['public_profile', 'email'],
-    });
-    if (type === 'success') {
-      const response = await fetch(
-        `https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert(
-        'Logged in!',
-        `Hi ${(await response.json()).name}!`,
-      );
-    }
-  }
 }
+
+const styles = StyleSheet.create({
+
+  containerView: {
+      flex: 1,
+  },
+  tinyLogo: {
+    width: 150,
+    height: 150,
+    marginLeft: 120,
+  },
+  loginScreenContainer: {
+      flex: 1,
+  },
+  iconStyle:{
+    position:"absolute",
+    top: 15,
+    left: 70,
+  },
+  logoText: {
+      fontSize: 33,
+      fontWeight: "800",
+      marginBottom: 30,
+      marginTop: -20,      
+      textAlign: 'center',
+  },
+  logoText2: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 30,
+    marginTop: -30,      
+    textAlign: 'center',
+},
+  loginFormView: {
+      flex: 1
+  },
+  loginFormTextInput: {
+      height: 55,
+      fontSize: 18,
+      borderWidth: 1,
+      borderColor: '#eaeaea',
+      backgroundColor: '#fafafa',
+      borderRadius: 25,
+      marginLeft: 50,
+      marginRight: 15,
+      marginBottom: 5,
+      width:300,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  loginButton: {
+      backgroundColor: 'red',
+      borderRadius: 5,
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 45,
+      marginTop: 10,
+      width: 300,
+      marginLeft:50,
+  },
+  fbLoginButton: {
+      height: 45,
+      marginTop: 10,
+      backgroundColor: 'transparent',
+  },
+});
